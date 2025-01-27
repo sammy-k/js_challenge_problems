e76291ed51126a02ee73f768fae06ebf0cae19c6
Sammy Kanan <samkanan@gmail.com>
2025-01-27 14:41:22 -0600

    complete roman numeral

--- easy/roman-numeral.js
+++ easy/roman-numeral.js
@@ -1,4 +1,4 @@
-class Roman {
+class RomanNumeral {
   constructor(number) {
     this.number = number;
   }
@@ -8,18 +8,76 @@ class Roman {
     let roman = '';
 
     while (num > 0) {
+      
       if (num >= 1000) {
         roman += 'M';
         num -= 1000;
       }
-      if (num >= 500) {
-        roman += 
+
+      else if (num >= 900) {
+        roman += 'CM';
+        num -= 900;
       }
+
+      else if (num >= 500) {
+        roman += 'D';
+        num -= 500;
+      }
+
+      else if (num >= 400) {
+        roman += 'CD';
+        num -= 400;
+      }
+
+      else if (num >= 100) {
+        roman += 'C';
+        num -= 100;
+      }
+
+      else if (num >= 90) {
+        roman += 'XC';
+        num -= 90;
+      }
+
+      else if (num >= 50) {
+        roman += 'L';
+        num -= 50;
+      }
+
+      else if (num >= 40) {
+        roman += 'XL';
+        num -= 40;
+      }
+
+      else if (num >= 10) {
+        roman += 'X';
+        num -= 10;
+      }
+
+      else if (num >= 9) {
+        roman += 'IX';
+        num -= 9;
+      }
+
+      else if (num >= 5) {
+        roman += 'V';
+        num -= 5;
+      }
+
+      else if (num >= 4) {
+        roman += 'IV';
+        num -= 4;
+      }
+
+      else if (num >= 1) {
+        roman += 'I';
+        num -= 1;
+      }
+     
     }
 
     return roman;
-
   }
 }
 
-module.exports = Roman;
+module.exports = RomanNumeral;
--- easy/sumOfMultiples.js
+++ easy/sumOfMultiples.js
@@ -1,7 +1,25 @@
-class sumOfMultiples {
-  constructor(num) {
-    this.num = num;
+class SumOfMultiples {
+  constructor(mult1, mult2) {
+    this.mult1 = mult1 || 3;
+    this.mult2 = mult2 || 5;
   }
+
+  to(num, mult1 = 3, mult2 = 5) {
+    let sum = 0;
+    for (let number = 1; number < num; number += 1) {
+      if (number % mult1 === 0 || number % mult2 === 0) {
+        sum += number;
+      }
+    }
+    return sum;
+  }
+
+  static to(num, mult1 = 3, mult2 = 5) {
+    return this.to(num, mult1, mult2);
+  }
+
 }
 
-module.exports = sumOfMultiples;
+console.log(SumOfMultiples.to(4))
+
+module.exports = SumOfMultiples;
